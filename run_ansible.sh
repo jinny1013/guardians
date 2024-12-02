b971c5aa7cf3de906b1e6b6d86414a83d6c0793f
root <root@ip-172-31-82-209.ec2.internal>
2024-12-02 08:13:22 +0000

    run_ansible.sh add to  run_scan.yml

--- run_ansible.sh
+++ run_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook -i /tmp/semaphore/repository_1_3/inventory.ini /tmp/semaphore/repository_1_3/ping_test.yml
+ansible-playbook -i /tmp/semaphore/repository_1_3/inventory.ini /tmp/semaphore/repository_1_3/run_scan.yml
 
